9fa59840804cca3758adb8ee0f78014a5d0ca1aa
F.R. van der Meulen <falco.meulen@gmail.com>
2019-06-19 11:40:24 +0200

    Comment changeup in main.

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,20 +18,23 @@
 int main() {
 	std::cout << "Main started!" << std::endl;
 	
-	//	Create subjects & Observers.
+	//	Create subject.
 	SudokuSubject *puzzle = new SudokuSubject();
+	
+	//	Create parser and pre-fill sudoku.
 	SudokuParser *parser = new SudokuParser(puzzle);
+	parser->parseSudoku("res/medium.txt");
 
-	//	Pre-fill sudoku.
-	parser->parseSudoku("res/expert.txt");
-
-	//	Create observers.
+	//	Create rule observers.
 	BoardObserver *board = new BoardObserver(puzzle);
 	RowObserver *rows = new RowObserver(puzzle);
 	ColumnObserver *columns = new ColumnObserver(puzzle);
 	BoxObserver *boxes = new BoxObserver(puzzle);
+	
+	//	Create filler.
 	FillObserver *filler = new FillObserver(puzzle);
 
+	//	Start solving.
 	puzzle->start();
 	return 0;
 }
